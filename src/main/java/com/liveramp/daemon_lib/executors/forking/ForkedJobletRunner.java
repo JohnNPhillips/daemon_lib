8ba63bb76822d714c3070dae364fb8f6dfe928db
Andreea Georgescu <ageorgescu@liveramp.com>
2016-01-21 18:13:07 -0800

    cleaned up ForkedJobletRunner

--- src/main/java/com/liveramp/daemon_lib/executors/forking/ForkedJobletRunner.java
+++ src/main/java/com/liveramp/daemon_lib/executors/forking/ForkedJobletRunner.java
@@ -1,54 +1,15 @@
 package com.liveramp.daemon_lib.executors.forking;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.Map;
-
-import com.google.common.io.ByteStreams;
-import org.apache.commons.io.FileUtils;
 
 import com.liveramp.daemon_lib.Joblet;
 import com.liveramp.daemon_lib.JobletConfig;
 import com.liveramp.daemon_lib.JobletFactory;
-import com.liveramp.daemon_lib.executors.processes.ProcessUtil;
 import com.liveramp.daemon_lib.tracking.DefaultJobletStatusManager;
 import com.liveramp.daemon_lib.utils.DaemonException;
 import com.liveramp.daemon_lib.utils.JobletConfigStorage;
 
-public class ForkedJobletRunner implements ProcessJobletRunner {
-  private static final String JOBLET_RUNNER_SCRIPT = "bin/joblet_runner.sh";
-  private static final String JOBLET_RUNNER_SCRIPT_SOURCE = "com/liveramp/daemon_lib/utils/joblet_runner.txt";
-
-  @Override
-  public int run(Class<? extends JobletFactory<? extends JobletConfig>> jobletFactoryClass, JobletConfigStorage configStore, String cofigIdentifier, Map<String, String> envVariables, String workingDir) throws IOException {
-    prepareScript();
-
-    ProcessBuilder processBuilder = new ProcessBuilder(JOBLET_RUNNER_SCRIPT, quote(jobletFactoryClass.getName()), configStore.getPath(), workingDir, cofigIdentifier);
-    processBuilder.environment().putAll(envVariables);
-    int pid = ProcessUtil.run(processBuilder);
-
-    return pid;
-  }
-
-  private static void prepareScript() throws IOException {
-    File productionScript = new File(JOBLET_RUNNER_SCRIPT);
-    if (!productionScript.exists()) {
-      InputStream scriptResourceInput = ForkedJobletRunner.class.getClassLoader().getResourceAsStream(JOBLET_RUNNER_SCRIPT_SOURCE);
-
-      FileUtils.forceMkdir(productionScript.getParentFile());
-      FileOutputStream scriptProductionOutput = new FileOutputStream(JOBLET_RUNNER_SCRIPT);
-
-      ByteStreams.copy(scriptResourceInput, scriptProductionOutput);
-
-      scriptResourceInput.close();
-      scriptProductionOutput.close();
-
-      productionScript.setExecutable(true);
-    }
-  }
-
+public class ForkedJobletRunner {
   public static String quote(String s) {
     return "'" + s + "'";
   }
