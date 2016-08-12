0f7427b6e708cdf076b55525afca3675b756b89a
Shreyas Kumar <skumar@liveramp.com>
2016-08-12 09:12:24 -0700

    mock postConfigExecutionCondition in testDaemon

--- test/java/com/liveramp/daemon_lib/TestDaemon.java
+++ test/java/com/liveramp/daemon_lib/TestDaemon.java
@@ -37,6 +37,7 @@ public class TestDaemon extends DaemonLibTestCase {
   @Test
   public void executeConfig() throws DaemonException {
     Mockito.when(preconfigExecutionCondition.canExecute()).thenReturn(true);
+    Mockito.when(postConfigExecutionCondition.canExecute(config)).thenReturn(true);
     Mockito.when(configProducer.getNextConfig()).thenReturn(config);
 
     daemon.processNext();
