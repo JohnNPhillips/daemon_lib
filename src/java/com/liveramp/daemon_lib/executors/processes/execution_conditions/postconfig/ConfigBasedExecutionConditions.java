b64d07321d19863df67531e10bb2b695bd5393c3
Shreyas Kumar <skumar@liveramp.com>
2016-08-22 11:44:19 -0700

    add ConfigBasedExecutionConditions#fromPredicate

--- src/java/com/liveramp/daemon_lib/executors/processes/execution_conditions/postconfig/ConfigBasedExecutionConditions.java
+++ src/java/com/liveramp/daemon_lib/executors/processes/execution_conditions/postconfig/ConfigBasedExecutionConditions.java
@@ -1,18 +1,30 @@
 package com.liveramp.daemon_lib.executors.processes.execution_conditions.postconfig;
 
+import com.google.common.base.Predicate;
+import com.google.common.base.Predicates;
 import com.liveramp.daemon_lib.JobletConfig;
 
 public class ConfigBasedExecutionConditions {
 
   public static <T extends JobletConfig> ConfigBasedExecutionCondition<T> alwaysExecute() {
-    return new AlwaysConfigBasedExecutionCondition<T>();
+    return fromPredicate(Predicates.<T>alwaysTrue());
   }
 
-  static class AlwaysConfigBasedExecutionCondition<T extends JobletConfig> implements ConfigBasedExecutionCondition<T> {
+  public static <T extends JobletConfig> ConfigBasedExecutionCondition<T> fromPredicate(Predicate<T> predicate) {
+    return new ConfigBasedExecutionConditionFromPredicate<T>(predicate);
+  }
+
+  static class ConfigBasedExecutionConditionFromPredicate<T extends JobletConfig> implements ConfigBasedExecutionCondition<T> {
+
+    private final Predicate<T> predicate;
+
+    ConfigBasedExecutionConditionFromPredicate(Predicate<T> predicate) {
+      this.predicate = predicate;
+    }
 
     @Override
     public boolean apply(T input) {
-      return true;
+      return predicate.apply(input);
     }
   }
 }
